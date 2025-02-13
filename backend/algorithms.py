d9988de25e44edbb9502aeeaf9fde37333b775a9
alain descartes <ualaindescartes@gmail.com>
2025-02-12 21:11:06 -0700

    initial project setup

---
+++ backend/algorithms.py
@@ -0,0 +1,39 @@
+
+unsortedArray = [7, 12, 9, 11, 3]
+
+
+def bubble_sort(array):
+    """
+        Sorts a list of numbers in ascending order using the Bubble Sort algorithm.
+
+        Parameters:
+            array (list): List of numbers to be sorted.
+
+        Returns:
+            list: The sorted list.
+        """
+    size = len(array)
+    while  size > 0:
+        swapped = False
+        for i in range(size - 1):
+            if array[i] > array[i+1]:
+                array[i], array[i+1] = array[i+1], array[i]
+                swapped = True
+
+        size -= 1
+        if not swapped:
+            break
+    return array
+
+def insertion_sort(array):
+   for current in range(1, len(array)):
+       temp = array[current]
+       position = current
+       while position > 0 and array[position-1] > temp:
+           array[position] = array[position-1]
+           position -= 1
+
+       array[position] = temp
+   print(array)
+   return array
+insertion_sort(unsortedArray)
