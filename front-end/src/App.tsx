4c6d280bfce0964ca35744ecb63005e000067136
alain descartes <ualaindescartes@gmail.com>
2025-02-17 22:21:01 -0700

    added the chart component

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,9 +1,16 @@
+import Chart from "./_components/Chart";
 import Header from "./_components/Header";
 
 function App() {
   return (
     <>
       <Header />
+      <main className="p-2 flex flex-col gap-4">
+        <div></div>
+        <div className="mt-64">
+          <Chart initialData={[1, 2, 3, 3]} />
+        </div>
+      </main>
     </>
   );
 }
