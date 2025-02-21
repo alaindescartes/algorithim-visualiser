978f668c455bf5aa5bb257312e30cd72950cbdbc
alain descartes <ualaindescartes@gmail.com>
2025-02-20 17:38:57 -0700

    added useSocket wrapper

--- front-end/src/_components/SocketListener.tsx
+++ front-end/src/_components/SocketListener.tsx
@@ -2,7 +2,7 @@ import useSocket from "@/util/useSocket";
 
 function SocketListener({ url }: { url: string }) {
   useSocket(url);
-  return null; // This component doesn't render any UI
+  return null;
 }
 
 export default SocketListener;
